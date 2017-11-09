43e33013fec9713c4acfe17d9aaaaef7f8062c94
Owen Shepherd <owsh8290@colorado.edu>
2017-11-09 13:36:37 -0700

    Re-arranged the example code into header files and removed the old ADXL files.

---
+++ LocalLib/TeensySDWrite.cpp
@@ -0,0 +1,66 @@
+#include "TeensySDWrite.h"
+#define SD_CS_PIN 4
+File myFile;
+
+void sdSetup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  String dataWrite = "testing 1,2,3.";
+
+
+  sdInitialize();
+
+  sdWrite(dataWrite);
+}
+
+void sdInitialize() {
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(SD_CS_PIN)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+
+  Serial.println("initialization done.");
+
+}
+
+void sdWrite(String data) {
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println(data);
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
