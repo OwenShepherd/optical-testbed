2f8dc9014c065db99feaa2b92652f0d550967a85
owsh8290@colorado.edu <owsh8290@colorado.edu>
2017-11-12 21:54:36 -0700

    Added a library to work with the DS18B20.  Currently giving a wrong value with 10kohm pullup - will try 4.7kohm ASAP.

---
+++ LocalLib/temp/temp.cpp
@@ -0,0 +1,25 @@
+#include "temp.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 1
+
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+
+void tempSetup() {
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  sensors.begin();
+}
+void tempLoop() {
+  Serial.print(" Requesting Temperatures... ");
+  sensors.requestTemperatures();
+  Serial.println("DONE");
+  Serial.print(" Temperature is: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  delay(1000);
+}
---
+++ LocalLib/temp/temp.h
@@ -0,0 +1,9 @@
+#ifndef TEMP_H
+#define TEMP_H
+
+
+void tempSetup();
+
+void tempLoop();
+
+#endif
