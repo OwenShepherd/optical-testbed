f29c338decc3c763fde9d33e1bcb2227d2ccaa79
OwenShepherd <owsh8290@colorado.edu>
2017-11-30 18:16:08 -0700

    Serial Read and Data Check
    Python script "readSerial.py" now reads from a teensy given some initial parameters.

--- serialRead.py
+++ serialRead.py
@@ -2,9 +2,13 @@ import serial
 import time
 
 inputs = ""
-testTime = 1
-Baud = 9600
-fileName = "C:\\Users\\sheph\\Documents\\Arduino\\ASEN-4018-EnvSensors" + "\\Teensy_"+str(Baud)+"_Baud_"+str(testTime)+"_seconds.txt"
+testTime = 1 # Total time you desire to test the Teensy
+Baud = 9600 # Baud rate (note on the Teensy this doesn't change Serial rates)
+Port = 'COM3' # USB port ID that the Teensy is plugged in to
+
+directory = "C:\\Users\\sheph\\Documents\\Arduino\\ASEN-4018-EnvSensors\\"
+fName = "Teensy_"+str(Baud)+"_Baud_"+str(testTime)+"_seconds.txt"
+fileName = directory+fName
 
 f = open(fileName,'w+')
 
@@ -16,12 +20,11 @@ byteCounter = 0
 
 
 
-with serial.Serial('COM3', Baud) as ser:
+with serial.Serial(Port, Baud) as ser:
     while ((end-start)<testTime):
         x = ser.read(1000)
         f.write(x.decode("utf-8"))
         byteCounter = byteCounter+1000
-        #print(str(x))
         end = time.time()
 
 f.close
@@ -30,4 +33,67 @@ timeElapsed = end-start
 print("Time Elapsed: " + str(timeElapsed))
 print("Bytes Read: " + str(byteCounter))
 print("Data Rate: " + str(byteCounter/timeElapsed/1000) + " kbps")
-print(fileName)
+print("Filename: "+ fName)
+
+
+
+with open(fileName) as f:
+    data = f.read()
+
+firstRead = True
+firstDigit = "1"
+secondDigit = "2"
+prevDigit = 12
+readFirst = False
+readSecond = False
+flags = 0
+counter = 0
+totalCounter = 0
+
+for c in data:
+
+    if (firstRead):
+        if (c==' '):
+            readFirst = True
+            continue
+        elif (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSecond):
+            secondDigit = c
+            readSecond = False
+            firstRead = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = prevDigit
+            totalCounter = totalCounter + 1
+
+    else:
+        if (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSecond):
+            secondDigit = c
+            readSecond = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = counter+1
+            totalCounter = totalCounter + 1
+            if (counter != prevDigit):
+                print()
+                print(totalCounter)
+                print(counter)
+                print(prevDigit)
+                input("Press Enter to continue...")
+                flags = flags + 1
+        else:
+            readFirst = True
+
+
+    if (counter==99):
+        counter = -1
+
+print("Error Flags: "+ str(flags))
+print("Count of Every Third Char: "+ str(totalCounter))
--- testErrorCheck.py
+++ testErrorCheck.py
@@ -1,24 +1,51 @@
+
+
 with open('TeensyTest.txt') as f:
     data = f.read()
 
+firstRead = True
 firstDigit = "1"
 secondDigit = "2"
 prevDigit = 12
 readFirst = False
 readSecond = False
 flags = 0
-counter = -100
+counter = 0
 
 for c in data:
 
-    prevDigit = int(firstDigit + secondDigit)
+    if (firstRead):
+        if (c==' '):
+            readFirst = True
+            continue
+        elif (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSeond):
+            secondDigit = c
+            readSecond = False
+            firstRead = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = prevDigit
 
-    if (readFirst):
-        firstDigit = c;
-        readFirst = False
-        readSecond = True
-    elif (readSecond):
-        secondDigit = c;
-        readSecond = False
-    elif (c==" " and counter = -100):
-        readFirst = true
+    else:
+        if (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSecond):
+            secondDigit = c
+            readSecond = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = counter+1
+            if (counter != prevDigit):
+                flags = flags + 1
+        else:
+            readFirst = True
+
+
+    if (counter==99):
+        counter = 0
