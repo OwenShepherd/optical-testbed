7b04a752245fb4e545a069755c32ffd594ce2513
OwenShepherd <owsh8290@colorado.edu>
2018-02-28 16:35:52 -0700

    Added a simple GUI to prompt the user to select a folder path
    The GUI includes a simple windows form with a "select directory" button and an "exit" button.  It's pretty rudimentary right now.

---
+++ WindowTesting/WindowTesting/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowTesting
+{
+    public partial class Form1 : Form
+    {
+
+        private string experimentPath;
+        private bool isSelected;
+
+        public Form1()
+        {
+            InitializeComponent();
+            experimentPath = "";
+            isSelected = false;
+        }
+
+        public string getSelectedPath()
+        {
+            return experimentPath;
+        }
+
+        public bool pathSelected()
+        {
+            return isSelected;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog expLoc = new FolderBrowserDialog();
+
+            if (expLoc.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                experimentPath = expLoc.SelectedPath;
+
+                isSelected = true;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WindowTesting/WindowTesting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sales
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            WindowTesting.Form1 formTest = new WindowTesting.Form1();
+
+            formTest.ShowDialog();
+
+            string userPath = formTest.getSelectedPath();
+
+            Console.WriteLine(formTest.getSelectedPath());
+           
+        }
+    }
+
+}
+
