87e885a8b56f68b6f8b5f0a277e14011802a025e
OwenShepherd <owsh8290@colorado.edu>
2018-03-02 12:19:53 -0700

    Added logo to GUI
    just so we can lol instead of cry while testing

--- WindowTesting/WindowTesting/ExperimentDialog.Designer.cs
+++ WindowTesting/WindowTesting/ExperimentDialog.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExperimentDialog));
             this.DirectoryBrowse = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
             this.ExperimentTitle = new System.Windows.Forms.Label();
@@ -38,7 +39,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.ExpDialog = new System.Windows.Forms.TextBox();
             this.StartButton = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // DirectoryBrowse
@@ -141,20 +141,11 @@
             this.StartButton.UseVisualStyleBackColor = true;
             this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(554, 225);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 10;
-            // 
             // ExperimentDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 272);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.StartButton);
             this.Controls.Add(this.ExpDialog);
             this.Controls.Add(this.label1);
@@ -165,6 +156,7 @@
             this.Controls.Add(this.ExperimentTitle);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.DirectoryBrowse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ExperimentDialog";
             this.Text = "AWESoMe";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -185,6 +177,5 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox ExpDialog;
         private System.Windows.Forms.Button StartButton;
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
