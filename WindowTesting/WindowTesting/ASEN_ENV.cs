bfdd2d01470d02a76257061abcd154635bef0b29
OwenShepherd <owsh8290@colorado.edu>
2018-03-08 23:43:11 -0700

    Filled out the methods in the ASEN_ENV class

--- WindowTesting/WindowTesting/ASEN_ENV.cs
+++ WindowTesting/WindowTesting/ASEN_ENV.cs
@@ -3,26 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
 
 namespace ASEN
 {
     class ASEN_ENV
     {
+        private bool READ;
+        private FileStream saveFile;
+        private SerialPort teensy;
+
         // Let's think of some things that we want...
         // Should be a relatively simple class
-        public ASEN_ENV()
+        public ASEN_ENV(string COMport, string directory)
         {
+            int baud = 2000000; // We can change this to anything.  Check out error rates and such for the Teensy 3.6 here: https://www.pjrc.com/teensy/td_uart.html
+            this.teensy = new SerialPort(COMport,baud);
 
+            // Appned the correct env csv path to the directory
+            string path = directory + "\\env_data.csv";
+
+            // Open the file and set the handle
+            this.saveFile = File.Open(path, FileMode.Open);
         }
 
+        // I need to set read separately from everything else for parallelization
+        public void PrepRead()
+        {
+            this.READ = true;
+        }
+
+        public void EndRead()
+        {
+            this.READ = false;
+        }
+
+        
         public void BeginTeensyRead()
         {
-
-        }
-
-        public void EndTeensyRead()
-        {
-
+            while (READ)
+            {
+                char theByte = (char)teensy.ReadByte();
+            }   
         }
     }
 }
