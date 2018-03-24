ad391ae30d61d53acbcc33499742a820ab4f9e96
OwenShepherd <owsh8290@colorado.edu>
2018-03-23 22:38:07 -0600

    Using serial read by launching python externally.  Much simpler than IronPython

---
+++ SerialTesting/SerialTesting/SerialTesting/ASEN_ENV.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+
+namespace ASEN
+{
+    class ASEN_ENV
+    {
+        private bool READ;
+        private FileStream saveFile;
+        private SerialPort teensy;
+        private byte[] readData = new byte[1];
+        public string path;
+
+        // Let's think of some things that we want...
+        // Should be a relatively simple class
+        public ASEN_ENV(string COMport, string directory)
+        {
+            int baud = 9600; // We can change this to anything.  Check out error rates and such for the Teensy 3.6 here: https://www.pjrc.com/teensy/td_uart.html
+            this.teensy = new SerialPort(COMport,baud);
+            //this.teensy.Open();
+
+            // Appned the correct env csv path to the directory
+            this.path = directory + "\\env_data.csv";
+        }
+
+        // I need to set read separately from everything else for parallelization
+        public void PrepRead()
+        {
+            this.READ = true;
+        }
+
+        public void EndRead()
+        {
+            this.READ = false;
+        }
+
+
+        public void StartListening()
+        {
+            // Subscribe to event and open serial port
+            this.teensy.DataReceived += new SerialDataReceivedEventHandler(Serial_DataReceived);
+            this.teensy.Open();
+        }
+
+        public void StopListening()
+        {
+            this.teensy.Close();
+        }
+
+        void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int dataLength = this.teensy.BytesToRead;
+            byte[] data = new byte[dataLength];
+            int nbrDataRead = this.teensy.Read(data, 0, dataLength);
+            if (nbrDataRead == 0)
+                return;
+            Console.WriteLine(Encoding.Default.GetString(data));
+            //File.WriteAllBytes(path, data);
+        }
+
+
+        
+        public void BeginTeensyRead(ref int dataCount)
+        {
+
+            this.teensy.DataReceived += (sender, e) =>
+            {
+                if (e.EventType == SerialData.Chars)
+                {
+
+                }
+            };
+            File.WriteAllBytes(path, readData);
+
+            //return dataCount;
+        }
+
+
+        
+
+
+        
+    }
+}
---
+++ SerialTesting/SerialTesting/SerialTesting/SerialReader.py
@@ -0,0 +1,130 @@
+﻿import serial
+import time
+
+inputs = ""
+Baud = 9600 # Baud rate (note on the Teensy this doesn't change Serial rates)
+maxLatency = 0
+
+
+testTime = 12 # Total time you desire to test the Teensy
+Port = 'COM3' # USB port ID that the Teensy is plugged in to
+
+# Directory to store the file and the filename are defined here, change for your
+# own filesystem
+directory = "C:\\Users\\sheph\\Documents\\ASEN-4018-Automation\\"
+fName = "Teensy_Test_"+str(testTime)+"_seconds.txt"
+fileName = directory+fName
+fileName2 = directory+"Teensy_Test_Statistics_"+str(testTime)+"_seconds.txt"
+
+f = open(fileName,'w+')
+
+start = time.time()
+end = time.time()
+byteCounter = 0
+
+# Here we read the serial port in 1000 byte blocks and writes to the above specified file
+with serial.Serial(Port, Baud) as ser:
+    while ((end-start)<testTime):
+        bytesToRead = ser.inWaiting()
+        startLatency = time.time()
+        x = ser.read(100)
+        endLatency = time.time()
+        currTime = endLatency-startLatency
+        if (currTime > maxLatency):
+            maxLatency = currTime
+        f.write(x.decode("utf-8"))
+        byteCounter = byteCounter+100
+        end = time.time()
+
+f.close
+timeElapsed = end-start
+
+
+# Data rate statistics
+print "Time Elapsed: " + str(timeElapsed)
+print "Bytes Read: " + str(byteCounter)
+print "Data Rate: " + str(byteCounter/timeElapsed/1000) + " kbps"
+print "Filename: "+ fName 
+
+
+# Opening up the file created above
+with open(fileName) as f:
+    data = f.read()
+
+firstRead = True
+firstDigit = "1"
+secondDigit = "2"
+prevDigit = 12
+readFirst = False
+readSecond = False
+flags = 0
+counter = 0
+totalCounter = 0
+
+
+# Iterating over each character in the collected data string
+for c in data:
+
+    # If it's the first read, want to make sure the counter is set to the first
+    # whole number transferred (sometimes the second half of a number is sent,
+    # these cases are ignored)
+    if (firstRead):
+        if (c==' '):
+            readFirst = True
+            continue
+        elif (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSecond):
+            secondDigit = c
+            readSecond = False
+            firstRead = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = prevDigit
+            totalCounter = totalCounter + 1
+
+    # This is everything not including the first read.  Keeps track of whether
+    # or not the upcoming byte to be read is the first or second digit of the
+    # space-separated number.  Also prints some error info if a flag is
+    # encountered
+    else:
+        if (readFirst):
+            firstDigit = c
+            readFirst = False
+            readSecond = True
+            continue
+        elif (readSecond):
+            secondDigit = c
+            readSecond = False
+            prevDigit = int(firstDigit+secondDigit)
+            counter = counter+1
+            totalCounter = totalCounter + 1
+            if (counter != prevDigit):
+                print ""
+                print totalCounter
+                print counter
+                print prevDigit
+                input("Press Enter to continue...")
+                flags = flags + 1
+        else:
+            readFirst = True
+
+    # Resetting counter.  Setting to -1 works best with the above code setup
+    if (counter==99):
+        counter = -1
+
+# Printing error info
+print "Error Flags: "+ str(flags) 
+print "Count of Every Third Char: "+ str(totalCounter)
+
+f2 = open(fileName2,'w')
+f2.write("Time Elapsed,Bytes Read,Data Rate,Filename,Flags,Char Count")
+f2.write("\n" + str(timeElapsed) + ",")
+f2.write(str(byteCounter) + ",")
+f2.write(str(byteCounter/timeElapsed/1000) + ",")
+f2.write(fName+",")
+f2.write(str(flags)+",")
+f2.write(str(totalCounter)+",")
+f2.close()
