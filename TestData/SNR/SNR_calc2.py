3d3329d45d82a3991503a7b78474f09118576fd3
OwenShepherd <owsh8290@colorado.edu>
2018-04-21 22:53:49 -0600

    SNR Calc close to completion

--- TestData/SNR/SNR_calc2.py
+++ TestData/SNR/SNR_calc2.py
@@ -30,6 +30,12 @@ def csv2png(file_in, saving=True):
     return holder.transpose()
 
 if __name__ == '__main__':
+    # image threshold
+    threshold = 2048
+    numStates = 8
+    numRows = 0
+    numCols = 0
+
     # Directories containing light dat
     lightDirs = [os.getcwd()] * 4
     lightDirs[0] += '\\SNR_LightOn'
@@ -38,13 +44,67 @@ if __name__ == '__main__':
     lightDirs[3] += '\\SNR_LightOff2'
 
     # Creating an array to hold the test data
-    testData = [[[0 for x in range(4)] for y in range(8)] for z in range(2)]
+    testData = [[[0 for x in range(2)] for y in range(8)] for z in range(4)]
+    countData = [[0 for y in range(5)] for x in range(8)]
 
     # Looping through every piece of test data
     for i in range(len(lightDirs)):
         # Collects all the state directories and the fore / aft files locations
         holder = glob(lightDirs[i] + "\\*\\data_RCWS\\*.csv")
 
-        # Going to loop through the holder names
+        # Going to loop through the holder names.
+        # Everything will be stored in the testData 3d array
         for j in range(0,len(holder),2):
-            state = j/2
+            state = int(j/2)
+            aftData = list(csv.reader(open(holder[j])))
+            foreData = list(csv.reader(open(holder[j+1])))
+            print("i: " + str(i) + "state: " + str(state))
+            testData[i][state][0] = aftData
+            numRows = len(aftData)
+            numCols = len(aftData[0])
+            testData[i][state][1] = foreData
+
+    # Looping through the states to collect the exposure times
+    header = []
+    print("Done")
+    holder = glob(lightDirs[0] + "\\*\\parameters.csv")
+    for i in range(len(holder)):
+        fileName = holder[i]
+        with open(fileName) as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            countData[i][0] = header[0]
+
+    # Now we've collected the test data as desired
+    # want to get the count data to hopefully back out the SNR
+    lightOn = []
+    lightOn2 = []
+    for i in range(len(lightDirs)):
+        # If we're dealing with a light on
+        print("Light #: " + str(i))
+        for state in range(numStates):
+            print("   State #: " + str(state))
+            aftData = testData[i][state][0]
+            foreData = testData[i][state][1]
+            if (i == 0 or i == 2):
+                for r in range(numRows):
+                    for c in range(numCols):
+                        if (aftData[r][c] != ''):
+                            data = int(aftData[r][c])
+                            if (i == 0):
+                                lightOn.append((r,c,data))
+                            else:
+                                lightOn2.append((r,c,data))
+                            countData[state][i+1] += data
+
+            elif (i == 1):
+                for item in lightOn:
+                    (r,c,data) = item
+                    countData[state][i+1] += int(aftData[r][c])
+
+            else:
+                for item in lightOn2:
+                    (r,c,data) = item
+                    countData[state][i+1] += int(aftData[r][c])
+
+    print(countData)
